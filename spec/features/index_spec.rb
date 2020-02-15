da9b2b93c12c811eedbce69031627d1a4f396817
Jordan Sewell <jrsewell400@gmail.com>
2020-02-15 16:49:24 -0700

    Update tests for links instead of content, and add test for create new article link

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -8,8 +8,9 @@ RSpec.describe "As a user,", type: :feature do
 
             visit '/articles'
 
-            expect(page).to have_content(article_1.title)
-            expect(page).to have_content(article_2.title)
+            expect(page).to have_link(article_1.title)
+            expect(page).to have_link(article_2.title)
+            expect(page).to have_link('Create a New Article')
         end
     end
 end
