69ca5e2bc70b583d0f266b461cc66ff44c31079a
Jordan Sewell <jrsewell400@gmail.com>
2020-02-15 17:12:54 -0700

    Fix show spec by adding missing do

--- spec/features/show_spec.rb
+++ spec/features/show_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "As a user," do 
     describe "if we click on an article title link" do
-        it "then we are taken to an individual article's show page." 
+        it "then we are taken to an individual article's show page." do
             article = Article.create!(title: "New Title", body: "New Body")
 
             visit '/articles'
