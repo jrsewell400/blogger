fcb40ba4caa1c59368ec167e576c85ea011b5332
Jordan Sewell <jrsewell400@gmail.com>
2020-02-17 21:32:24 -0700

    Moved folder

---
+++ spec/features/articles/index_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "As a user,", type: :feature do 
+    describe 'if I visit /artciles' do 
+        it "then I see a display of all articles." do
+            article_1 = Article.create!(title: "Title 1", body: "Body 1")
+            article_2 = Article.create!(title: "Title 2", body: "Body 2")
+
+            visit '/articles'
+
+            expect(page).to have_link(article_1.title)
+            expect(page).to have_link(article_2.title)
+            expect(page).to have_link('Create a New Article')
+        end
+    end
+end
